2312d3745e2518a15a609082f9a3172217e3ec89
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2020-08-02 17:24:57 +0200

    feat: added load-tests project

---
+++ load-tests/pages/table.js
@@ -0,0 +1,37 @@
+import {useReport} from "../utils/useReport"
+import ReportTable from "../components/ReportTable"
+import EmptyPlaceholder from "../components/EmptyPlaceholder"
+
+
+export default()=>{
+  const [report, _] = useReport()
+  const header=['title','2xx', 'non2xx','duration','connections', 'finish']
+
+  const tableSection = () =>{
+    if (!report){
+      return (
+        <section>
+          <dv4-loader-timer>Loading message...</dv4-loader-timer>
+        </section>
+      )
+    }else if (report.length===0){
+      return <EmptyPlaceholder />
+    }else{
+      return (
+        <section>
+          <ReportTable
+            header = {header}
+            data = {report}
+          />
+        </section>
+      )
+    }
+  }
+
+  return (
+    <article>
+      <h1>Todo api load tests table</h1>
+      {tableSection()}
+    </article>
+  )
+}
